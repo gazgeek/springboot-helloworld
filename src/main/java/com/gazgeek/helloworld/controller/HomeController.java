d8cd86cf5e294be9e114eb80d58235d99deaec6b
Hengchao Wang <whc0721a@vip.qq.com>
2019-11-17 02:56:34 -0600

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek hellllll!";
+        return "Hello from GazGeek hellll!";
     }
 
 }
