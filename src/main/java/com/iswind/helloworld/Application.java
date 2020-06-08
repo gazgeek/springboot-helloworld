1769df940ea9b75d0ab92f137623a0c024db42f8
isukkang <66412532+isukkang@users.noreply.github.com>
2020-06-08 16:34:41 +0900

    Create Application.java

---
+++ src/main/java/com/iswind/helloworld/Application.java
@@ -0,0 +1,17 @@
+package com.iswind.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
