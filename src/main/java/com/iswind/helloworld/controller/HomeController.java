40d4edabe6a308206b647fbc365803989a6b0c2b
isukkang <66412532+isukkang@users.noreply.github.com>
2020-06-08 16:36:50 +0900

    Create HomeController.java

---
+++ src/main/java/com/iswind/helloworld/controller/HomeController.java
@@ -0,0 +1,15 @@
+  
+package com.gazgeek.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello from GazGeek!";
+    }
+
+}
